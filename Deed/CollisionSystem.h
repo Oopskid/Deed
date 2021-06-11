6ce08221821eb86fc079e0b4828100c931766c05
Adam <ahughesformal@gmail.com>
2021-06-11 02:11:50 +0100

    More collision code

--- Deed/CollisionSystem.h
+++ Deed/CollisionSystem.h
@@ -22,9 +22,8 @@ namespace Collision
 
 		void solve(T time, Manager* manager)
 		{
-
-			std::multimap<T, std::pair<ColPair, size_t>>
-
+			CEvents events;
+			populate(time, events);
 
 		}
 
@@ -44,6 +43,49 @@ namespace Collision
 		}
 
 		private:
+		typedef std::multimap<T, std::pair<ColPair, size_t>> CEvents;
+
+		//Check all pairs and queue imminent events
+		void populate(T time, CEvents& events)
+		{
+			size_t curId = 0;
+			for (size_t xObj = 1; xObj < collidables.size(); xObj++)
+			{
+				Obj* first = collidables[xObj];
+				for (size_t yobj = 0; yObj < xObj; yObj++)
+				{
+					Obj* second = collidables[yObj];
+
+					considerCollision(time, first, second, curId, events);
+
+					curid++; //Next pair
+				}
+			}
+		}
+
+		bool considerCollision(T time, Obj* first, Obj* second, size_t id, CEvents& events)
+		{
+			//Lightweight collision detection
+			if (asPair(rules).canCollide(time, first, second))
+			{
+				//Passed, now for the hard part
+				T result = time;
+				ColPair collision;
+
+				if (asPair(collision).hardCollision(result, first, second))
+				{
+					//Valid collision
+					if (result < time)
+					{
+						//Collision is imminent, may occur!
+						events.insert(std::make_pair(result, std::make_pair(collision, id)));
+						return true;
+					}
+				}
+			}
+			return false;
+		}
+
 		//Gets the objs associated with a pair ID
 		void getPair(size_t id, Obj*& first, Obj*& second)
 		{
@@ -52,6 +94,11 @@ namespace Collision
 			//Imagine a coordinate system of an (n-1)x(n-1) triangle, where the id is the 1-d index
 			first = collidables[it->second];
 			second = collidables[id - it->first];
+		}
+
+		inline Pair<Obj, Obj, Manager, T>& asPair(Colpair& pair)
+		{
+			return static_cast<Pair<Obj, Obj, Manager, T>>(pair);
 		}
 
 		ColPair rules;
